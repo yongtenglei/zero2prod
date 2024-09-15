3586be276bd90844139a928985a7ac0f81251dd8
yongtenglei <yongtengrey@outlook.com>
2024-09-15 22:53:33 +0100

    First unit test

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
+use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn health_check(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .bind("127.0.0.1:8000")?
-        .run()
-        .await
+        .run();
+
+    Ok(server)
 }
