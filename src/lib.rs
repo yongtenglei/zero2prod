a3cb665754306e3bc35a51ef33ad79d20ca0d934
yongtenglei <yongtengrey@outlook.com>
2024-09-15 23:57:16 +0100

    Endpoint subscriptions

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,24 @@
 use std::net::TcpListener;
 
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
 
-async fn health_check(_req: HttpRequest) -> impl Responder {
+async fn health_check(_req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
